377d81b03bb0cefadced86a5b4996d60a6941677
Josh Belanich <josh@diffeo.com>
2014-09-19 09:18:33 -0400

    Added ido-mode

--- init.el
+++ init.el
@@ -9,9 +9,23 @@
 (tool-bar-mode -1)
 (which-function-mode 1)
 
+(require 'ido)
+(ido-mode t)
 
 ;;This sets .cc.fsm files to use c++-mode
 (setq auto-mode-alist (cons '("\\.cc.fsm$" . c++-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.m$" . octave-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.h$" . c++-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.h.fsm$" . c++-mode) auto-mode-alist))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ido-enable-flex-matching t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
